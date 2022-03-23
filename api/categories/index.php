44207a50c4e854a182943d833eba6695e295a1eb
Taylor <tamynhier@mail.fhsu.edu>
2022-03-23 19:14:33 -0400

    added headers to help Heroku

--- api/categories/index.php
+++ api/categories/index.php
@@ -7,6 +7,7 @@
     if ($method === 'OPTIONS') {
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        header("HTTP/1.1 200 OK");
     }
 
     include_once '../../config/database.php';
