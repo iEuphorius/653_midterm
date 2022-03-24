fec513921e677aaaf9f5b8c4229f03086af25475
Taylor <tamynhier@mail.fhsu.edu>
2022-03-23 20:53:06 -0400

    attempted to fix create-quote output, added missing param for author

--- api/authors/create.php
+++ api/authors/create.php
@@ -12,18 +12,22 @@ $author = new Author($db);
 
 // get raw author data
 $data = json_decode(file_get_contents("php://input"));
-$author->author = $data->author;
-
-// create author
-if($author->create()){
-    $lastId = $db->lastInsertId();
-    $message = array(
-        'id' => $lastId,
-        'author' => $author->author
-    );
-    echo json_encode($message);
+if($data->author == null){
+    echo json_encode(array('message'=> 'Missing Required Parameters'));
 } else {
-    echo json_encode(
-        array('message'=> 'Author not Created')
-    );
+    $author->author = $data->author;
+
+    // create author
+    if($author->create()){
+        $lastId = $db->lastInsertId();
+        $message = array(
+            'id' => $lastId,
+            'author' => $author->author
+        );
+        echo json_encode($message);
+    } else {
+        echo json_encode(
+            array('message'=> 'Author not Created')
+        );
+    }
 }
--- api/authors/update.php
+++ api/authors/update.php
@@ -13,19 +13,24 @@ $author = new Author($db);
 // get raw category data
 $data = json_decode(file_get_contents("php://input"));
 
-// set ID to update
-$author->id = $data->id;
-$author->author = $data->author;
-
-// update author
-if($author->update()){
-    $message = array(
-        'id' => $author->id,
-        'author' => $author->author
-    );
-    echo json_encode($message);
+if($data->author == null){
+    echo json_encode(array('message'=> 'Missing Required Parameters'));
 } else {
-    echo json_encode(
-        array('message'=> 'Author not Updated')
-    );
+
+    // set ID to update
+    $author->id = $data->id;
+    $author->author = $data->author;
+
+    // update author
+    if($author->update()){
+        $message = array(
+            'id' => $author->id,
+            'author' => $author->author
+        );
+        echo json_encode($message);
+    } else {
+        echo json_encode(
+            array('message'=> 'Author not Updated')
+        );
+    }
 }
